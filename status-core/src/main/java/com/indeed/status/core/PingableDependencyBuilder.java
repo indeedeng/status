6e2011621056e04a3a4c2110f5e213e1c92f30a1
Cameron Davison <cameron@indeed.com>
2014-02-26 10:23:51 -0600

    COMMON-722 : Attempting to keep binary compatibility

--- status-core/src/main/java/com/indeed/status/core/PingableDependencyBuilder.java
+++ status-core/src/main/java/com/indeed/status/core/PingableDependencyBuilder.java
@@ -1,5 +1,9 @@
 package com.indeed.status.core;
 
+import com.google.common.base.Supplier;
+
+import javax.annotation.Nonnull;
+
 /**
  * @author matts
  */
@@ -7,4 +11,34 @@ public abstract class PingableDependencyBuilder<T extends PingableDependency, B 
     extends AbstractDependencyBuilder<T, B>
 {
     protected PingableDependencyBuilder() {}
+
+    @Override
+    public B setId(String id) {
+        return super.setId(id);
+    }
+
+    @Override
+    public B setDescription(String description) {
+        return super.setDescription(description);
+    }
+
+    @Override
+    public B setTimeout(long timeout) {
+        return super.setTimeout(timeout);
+    }
+
+    @Override
+    public B setPingPeriod(long pingPeriod) {
+        return super.setPingPeriod(pingPeriod);
+    }
+
+    @Override
+    public B setUrgency(Urgency urgency) {
+        return super.setUrgency(urgency);
+    }
+
+    @Override
+    public B setToggle(@Nonnull Supplier<Boolean> toggle) {
+        return super.setToggle(toggle);
+    }
 }
