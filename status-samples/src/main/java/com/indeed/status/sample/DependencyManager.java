62af029afb38c32a1ca3762769737c8d295c9d5f
Jeremy Pitzeruse <pitz@indeed.com>
2015-07-08 17:36:42 -0500

    Implemented samples

---
+++ status-samples/src/main/java/com/indeed/status/sample/DependencyManager.java
@@ -0,0 +1,12 @@
+package com.indeed.status.sample;
+
+import com.indeed.status.core.AbstractDependencyManager;
+
+/**
+ * @author pitz@indeed.com (Jeremy Pitzeruse)
+ */
+public class DependencyManager extends AbstractDependencyManager {
+    public DependencyManager() {
+        super("Status-Sample");
+    }
+}
