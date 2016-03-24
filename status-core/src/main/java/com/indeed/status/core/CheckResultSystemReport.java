3300d271e0059953824a4343f9f8e6305fd82e27
Chen Yang <chen@indeed.com>
2016-03-24 10:58:50 -0500

    COMMON1597 Add javadoc for the marker interface +review CR-33463

--- status-core/src/main/java/com/indeed/status/core/CheckResultSystemReport.java
+++ status-core/src/main/java/com/indeed/status/core/CheckResultSystemReport.java
@@ -1,5 +1,8 @@
 package com.indeed.status.core;
 
+/**
+ * it's a marker interface intended to be implemented by json-serializable objects that will comprise the status report.
+ */
 public interface CheckResultSystemReport {
 
 }
