bfff0db2a4c8e2b753ff97e28ad3b94f484a41fd
matts <matts@indeed.com>
2016-05-19 21:38:38 -0700

    STATUS-4 - Add a testcase covering json creation via a background pinger

--- status-core/src/test/java/com/indeed/status/core/CheckResultSystemReportTest.java
+++ status-core/src/test/java/com/indeed/status/core/CheckResultSystemReportTest.java
@@ -7,13 +7,16 @@ import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
 import com.google.common.io.Resources;
 import com.indeed.util.core.time.StoppedClock;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -24,11 +27,10 @@ import static org.junit.Assert.assertEquals;
  *
  */
 public class CheckResultSystemReportTest {
-    private final StoppedClock wallClock = new StoppedClock();
-    private final SystemReporter systemReporter = new SystemReporter(wallClock);
-
     @Test
     public void testSimpleReport() throws IOException {
+        final StoppedClock wallClock = new StoppedClock();
+        final SystemReporter systemReporter = new SystemReporter(wallClock);
         final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("US/Chicago"));
         calendar.set(2016, 01, 01);
         final Date sampleDate = calendar.getTime();
@@ -76,6 +78,8 @@ public class CheckResultSystemReportTest {
 
     @Test
     public void testDetailedReport() throws IOException {
+        final StoppedClock wallClock = new StoppedClock();
+        final SystemReporter systemReporter = new SystemReporter(wallClock);
         final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT-6"));
         calendar.setTimeInMillis(0);
         calendar.set(2016, Calendar.JANUARY, 1);
@@ -129,6 +133,73 @@ public class CheckResultSystemReportTest {
                 expectedJson, json);
     }
 
+    @Test
+    public void testDetailedReportBackground() throws IOException, InterruptedException {
+        final StoppedClock wallClock = new StoppedClock();
+        final SystemReporter systemReporter = new SystemReporter(wallClock);
+        final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT-6"));
+        calendar.setTimeInMillis(0);
+        calendar.set(2016, Calendar.JANUARY, 1);
+        final Date sampleDate = calendar.getTime();
+        wallClock.set(sampleDate.getTime());
+
+        final SimpleDependencyManager manager = new SimpleDependencyManager(
+                "app",
+                null,
+                systemReporter);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        final SimplePingableDependency dependency = SimplePingableDependency.newBuilder()
+                .setId("id")
+                .setDescription("description")
+                .setPingMethod(new PingMethod() {
+                    @Override
+                    public void ping() throws Exception {
+                        // Simulate taking 10 ms to complete.
+                        wallClock.plus(10, TimeUnit.MILLISECONDS);
+                        latch.countDown();
+                    }
+                })
+                .setWallClock(wallClock)
+                .build();
+
+        manager.launchPinger(dependency);
+        latch.await(10, TimeUnit.SECONDS);
+
+        final CheckResultSet resultSet = manager.evaluate();
+
+        assertEquals(
+                "Expected the timestamp of the result set to match the time of the wall clock at the moment the " +
+                        "result set was created, i.e. after the background ping has occurred.",
+                sampleDate.getTime() + 10, resultSet.getStartTimeMillis());
+
+        final CheckResult checkResult = resultSet.get("id");
+        assert checkResult != null;
+
+        assertEquals(
+                "Expected the 'last executed' timestamp of the individual dependency to record the time " +
+                        "the evaluation of dependency was initiated.",
+                sampleDate.getTime(), checkResult.getTimestamp());
+
+        final CheckResultSystemReport report = resultSet.summarizeBySystemReporter(true);
+
+        final String json = new ObjectMapper()
+                .addMixIn(Object.class, PropertyFilterMixIn.class)
+                .writer()
+                .with(new SimpleFilterProvider()
+                        .addFilter("master-filter", serializeAllExcept("hostname")))
+                .withDefaultPrettyPrinter()
+                .writeValueAsString(report);
+
+        final String expectedJson = Resources.toString(
+                Resources.getResource(getTestResourcePath() + "/testDetailedReportBackground.json"),
+                Charsets.UTF_8);
+        assertEquals(
+                "Failed to generate the expected json report",
+                expectedJson, json);
+    }
+
+
     @Nonnull
     private String getTestResourcePath() {
         return getClass().getName().replace('.', '/');
@@ -136,4 +207,14 @@ public class CheckResultSystemReportTest {
 
     @JsonFilter("master-filter")
     class PropertyFilterMixIn {}
+
+    private static class SimpleDependencyManager extends AbstractDependencyManager {
+        public SimpleDependencyManager(
+                @Nullable final String appName,
+                @Nullable final Logger logger,
+                @Nonnull final SystemReporter systemReporter
+        ) {
+            super(appName, logger, systemReporter);
+        }
+    }
 }
